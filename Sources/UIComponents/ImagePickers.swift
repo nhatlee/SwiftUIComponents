6f782c8dd0cbcdb2eba4d254ec1b874995736fe5
NhatLe <lenhathk@gmail.com>
2025-01-12 15:07:21 +0700

    add single image picker

---
+++ Sources/UIComponents/ImagePickers.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  UIComponents
+//
+//  Created by Nhat Le on 12/1/25.
+//
+
+import SwiftUI
+import PhotosUI
+
+@Observable final class ImagePickers {
+    var image: Image?
+    var imageSelection: PhotosPickerItem? {
+        didSet {
+            guard let imageSelection else { return }
+            Task { try await loadTransferable(from: imageSelection)}
+        }
+    }
+    
+    func loadTransferable(from imageSelection: PhotosPickerItem) async throws {
+        do {
+            guard let image = try await imageSelection.loadTransferable(type: Image.self) else { return }
+            self.image = image
+        } catch {
+            print(error.localizedDescription)
+            image = nil
+        }
+    }
+}
