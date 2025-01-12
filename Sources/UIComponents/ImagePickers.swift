4811665b29f22b41ff5cb0aabd0872416b4dd165
NhatLe <lenhathk@gmail.com>
2025-01-12 16:01:14 +0700

    [ImagePicker] Add multiple images pickers

--- Sources/UIComponents/ImagePickers.swift
+++ Sources/UIComponents/ImagePickers.swift
@@ -8,22 +8,49 @@
 import SwiftUI
 import PhotosUI
 
+@MainActor
 @Observable public final class ImagePickers {
-    var image: Image?
-    var imageSelection: PhotosPickerItem? {
+    public init() {}
+    /// Single image selection
+    public var image: Image?
+    public var imageSelection: PhotosPickerItem? {
         didSet {
             guard let imageSelection else { return }
             Task { try await loadTransferable(from: imageSelection)}
         }
     }
+    /// Multiple image selection
+    public var images: [Image] = []
+    public var imageSelections: [PhotosPickerItem] = [] {
+        didSet {
+            Task {
+                guard !imageSelections.isEmpty else { return }
+                try await loadTransferable(from: imageSelections)
+                imageSelections = []
+            }
+        }
+    }
     
     func loadTransferable(from imageSelection: PhotosPickerItem) async throws {
         do {
-            guard let image = try await imageSelection.loadTransferable(type: Image.self) else { return }
-            self.image = image
+            guard let imageData = try await imageSelection.loadTransferable(type: Data.self),
+            let uiImage = UIImage(data: imageData) else { return }
+            self.image = Image(uiImage: uiImage)
         } catch {
             print(error.localizedDescription)
             image = nil
+        }
+    }
+    
+    func loadTransferable(from imageSelections: [PhotosPickerItem]) async throws {
+        do {
+            for imageSelection in imageSelections {
+                guard let imageData = try await imageSelection.loadTransferable(type: Data.self),
+                      let uiImage = UIImage(data: imageData) else { return }
+                self.images.append(Image(uiImage: uiImage))
+            }
+        } catch {
+            print(error.localizedDescription)
         }
     }
 }
